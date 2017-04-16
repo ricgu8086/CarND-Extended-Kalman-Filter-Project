6371a082c885d3e7034d692fb70dc2b0a8022294
ricgu8086 <ricgu8086@gmail.com>
2017-04-16 20:33:36 +0200

    Used Eclipse auto-format tool to properly align the code.

--- src/tools.cpp
+++ src/tools.cpp
@@ -5,21 +5,27 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {}
-
-Tools::~Tools() {}
-
-VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+Tools::Tools()
+{
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+Tools::~Tools()
+{
+}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+		const vector<VectorXd> &ground_truth)
+{
+	/**
+	 TODO:
+	 * Calculate the RMSE here.
+	 */
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state)
+{
+	/**
+	 TODO:
+	 * Calculate a Jacobian here.
+	 */
 }
